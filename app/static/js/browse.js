c1041c8ce46f8fc607d1bb331e0e5d80d83ab814
Xavier Carmo <xavier_carmo@yahoo.com.au>
2019-05-19 14:31:55 +1000

    browse view update
    added a fade-in effect for browse view so that items appear once the page is fully loaded (including JS dependencies)

--- app/static/js/browse.js
+++ app/static/js/browse.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
     $('.js-example-basic-multiple').select2({ theme: "bootstrap", width: "resolve" });
+    $('#filters-container').css('opacity', 1);
+    $('#movies-container').css('opacity', 1);
 });
