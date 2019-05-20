32ec6d0fb8f80221dafa4d90edaa1aee8eda0c60
kingdion <dion.misic@gmail.com>
2019-05-20 23:12:49 +1000

    Incremental work on login/authentication

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.update(
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:isd_password@localhost:5432/isd",
-    SQLALCHEMY_TRACK_MODIFICATIONS = True,
-    SECRET_KEY = "Change in production",
-    DEBUG = True,
-)
+debugMode = True
+
+if debugMode:
+    app.config.update(
+        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:isd_password@localhost:5432/isd",
+        SQLALCHEMY_TRACK_MODIFICATIONS = True, 
+        SECRET_KEY = "Change in production",
+        SESSION_COOKIE_SECURE = False,
+        DEBUG = True,
+    )
+else:
+    app.config.update(
+        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:isd_password@localhost:5432/isd",
+        SQLALCHEMY_TRACK_MODIFICATIONS = False, 
+        SECRET_KEY = "C94222125//34/rtgtrwrubwui24984524nfejsrjbf5", # randomly generated string
+        SESSION_COOKIE_SECURE = True,
+        DEBUG = False,
+    )
 
 db = SQLAlchemy(app)
 
 from .routes import routes
+from .authentication import auth
 
 app.register_blueprint(routes)
+app.register_blueprint(auth)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import render_template, flash, redirect, url_for, request, jsonify
 from . import db
+from .authentication import protected_view
 from .models import Movie, Genre, MovieGenre, Account, MovieOrderLine, Orders
 
 routes = Blueprint("routes", __name__)
@@ -9,48 +10,10 @@ routes = Blueprint("routes", __name__)
 def index():
     return render_template("index.html")
 
-@routes.route("/login")
-def login():
-    return render_template("login.html")
-
-@routes.route("/do-login")
-def do_login():
-    return redirect(url_for('routes.dashboard'))
-
-def do_login_manual(username, password):
-    pass
-
-@routes.route("/register")
-def register():
-    return render_template("register.html")
-
-@routes.route("/do-register", methods=["POST"])
-def do_register():
-    email_exists = db.session.query(Account.email).filter_by(email=request.form["email"]).scalar() is not None
-
-    if email_exists:
-        return jsonify({"success": False, "reason": "email exists"})
-
-    account = Account(\
-        first_name=request.form["first-name"],\
-        last_name=request.form["last-name"],\
-        email=request.form["email"],\
-        password=request.form["password"],\
-        street_address=request.form["street-address"],\
-        postcode=request.form["postcode"],\
-        is_staff=False\
-    )
-
-    # TODO: Add server-side validation (since clients can just alter the javascript to bypass client-side validation)
-
-    db.session.add(account)
-    db.session.commit()
-
-    # TODO: Return login() or whatever, Dion will implement later
-    return jsonify({"success": True})
-
 @routes.route("/dashboard")
-def dashboard():
+@protected_view
+def dashboard(current_user):
+    print(current_user)
     return render_template("dashboard.html")
 
 @routes.route("/browse")
