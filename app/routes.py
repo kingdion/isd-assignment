27e2067c5b49c74420fddb3bf28820c58e208b87
Xavier Carmo <xavier_carmo@yahoo.com.au>
2019-05-21 12:13:09 +1000

    resolving conflicts

--- app/routes.py
+++ app/routes.py
@@ -22,17 +22,22 @@ def browse():
 
 @routes.route("/do-get-movies", methods=["POST"])
 def do_get_movies():
-    genres = request.form.getlist('genres[]')
-    years = request.form.getlist('years[]')
-    ratings = request.form.getlist('ratings[]')
-    print(genres, years, ratings, request.form["test"])
+    genres = request.form.getlist("genres[]")
+    years = request.form.getlist("years[]")
+    ratings = request.form.getlist("ratings[]")
+    index = request.form["index"]
+    length = request.form["amount"]
+
+    movies = db.session.query(Movie)
+    print(movies) #use sql alchemy slice
+
     return jsonify({"success": False})
 
 @routes.route("/do-add-movie")
 def do_add_movie():
     movie_exists = False #later this will actually check
 
-    if (movie_exists):
+    if movie_exists:
         return jsonify({"success": False, "reason": "movie exists"})
 
     movie = Movie(\
@@ -40,11 +45,25 @@ def do_add_movie():
         releaseDate = request.form["release-date"],\
         thumbnailSrc = "static/images/image.png",\
         runtime = request.form["runtime"],\
-        maturity_rating = request.form["maturity-rating"]
+        maturity_rating = int(request.form["maturity-rating"]) #assumes the client has gotten list of maturity ratings
         #genre? not sure how alchemy works with associative entities, should ask dion
     )
+
+    for genre in request.form.getlist("genres[]"):
+        email_exists = db.session.query(Account.email).filter_by(email=request.form["email"]).scalar() is not None
+        dbGenreID = db.session.query(Genre).filter_by(name=genre).one()
+        movie.genres.append(dbGenreID)
 
     db.session.add(movie)
     db.session.commit()
 
-    return jsonify({"success": False})
+    return jsonify({"success": True})
+
+@routes.route("/do-get-genres", methods=["GET"])
+def do_get_genres():
+    result = []
+    genres = db.session.query(Genre).all()
+    for genre in genres:
+        result.append(genre.__dict__)
+
+    return jsonify(result)
