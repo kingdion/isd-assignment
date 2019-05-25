b00c1fe3e311c9402de890188d7322c8ae4c87f8
Xavier Carmo <xavier_carmo@yahoo.com.au>
2019-05-25 20:50:42 +1000

    fixed up adding/editing movies

--- app/static/js/add_edit_movie.js
+++ app/static/js/add_edit_movie.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     $('select').select2({ theme: "bootstrap", width: "resolve" });
     $('[name="genres[]"]').select2({ placeholder: "Genres", theme: "bootstrap", width: "resolve" });
-    $('[data-toggle="tooltip"]').tooltip({ trigger: 'hover' });
     $('#body-content').css('opacity', 1);
+
+    $('[name="title"]').focusout(function(){ validate_not_empty($(this)); });
+    $('[name="release-date"]').focusout(function(){ validate_not_empty($(this)); });
+    $('[name="runtime"]').focusout(function(){ validate_not_empty($(this)); });
 });
 
 $('#movie-form').submit(function(event) {
@@ -15,5 +18,36 @@ function add_edit_movie_callback(data) {
         alert("Movie successfully added!");
         $('#movie-form')[0].reset();
         $('select').trigger('change');
+    }
+    else {
+        if (data.reason == 'movie exists') {
+            set_warning($('[name="title"]'), "A movie with that title already exists");
+        }
+    }
+}
+
+function set_warning(el, warning) {
+    el.tooltip('hide')
+      .css('border', '1px solid red')
+      .prop('title', warning)
+      .attr('data-original-title', warning)
+      .tooltip('show');
+}
+
+function clear_warning(el) {
+    el.css('border', '1px solid #ced4da')
+      .prop('title', '')
+      .attr('data-original-title', '')
+      .tooltip('hide');
+}
+
+function validate_not_empty(el) {
+    if (el.val().length == 0) {
+        set_warning(el, 'Required field.');
+        return false;
+    }
+    else {
+        clear_warning(el);
+        return true;
     }
 }
