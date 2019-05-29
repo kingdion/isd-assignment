db32907f712761b4c7aa7fe4946735ef65963023
kingdion <dion.misic@gmail.com>
2019-05-30 09:15:50 +1000

    Added a base db setup create script, creates 20 user/staff f

---
+++ setup_mock_db.py
@@ -0,0 +1,57 @@
+import datetime
+from app import create_app
+from app.models import *
+from werkzeug.security import generate_password_hash, check_password_hash
+
+print("----")
+print("This will drop all existing rows and replace them with test rows.") 
+print("----")
+print("Are you sure you want to do this? (Y / N)")
+answer = input("> ")
+
+if (answer == "Y"):
+    print("Started creation process..")
+    app = create_app()
+    with app.app_context():
+            db.drop_all()
+            db.create_all()
+
+            for current_id in range(21):
+                acc = Account(
+                    f"Test-{current_id}", 
+                    f"User-{current_id}", 
+                    f"testuser{current_id}@gmail.com",
+                    f"TestUser{current_id}",
+                    generate_password_hash(f"TestUser{current_id}", method='sha256'),
+                    "Fake Address at Fake Street",
+                    "2020",
+                    "0400 000 000",
+                    False,
+                    True,
+                    datetime.datetime.utcnow()
+                )
+
+                print("Created: ", acc)
+                db.session.add(acc)
+
+            for current_id in range(21):
+                staff = Staff(
+                    f"StaffTest-{current_id}", 
+                    f"User-{current_id}", 
+                    f"stafftestuser{current_id}@gmail.com",
+                    f"StaffTestUser{current_id}",
+                    generate_password_hash(f"StaffTestUser{current_id}", method='sha256'),
+                    "Fake Address at Fake Street",
+                    "2020",
+                    "0400 000 000",
+                    True,
+                    True,
+                    datetime.datetime.utcnow()
+                )
+
+                print("Created: ", acc)
+                db.session.add(acc)
+
+            print("20 accounts created")
+            print("20 staff created")
+            db.session.commit()
