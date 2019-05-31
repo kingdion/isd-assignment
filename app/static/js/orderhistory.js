e983d8a48bfb2f1258ec26f85afcd907d8932b77
atut2000 <44915179+atut2000@users.noreply.github.com>
2019-05-31 22:37:30 +1000

    test

--- app/static/js/orderhistory.js
+++ app/static/js/orderhistory.js
@@ -1,42 +1,46 @@
-$("#date-time-picker-button").click(function(event)
+// $("#date-time-picker-button").click(function(event)
+// {
+//     // Get the datepicker value
+//     var dateToFilterBy = $("#date-time-picker").val();
+//
+//     // For each log's timestamp, check if the date of the row
+//     // is equal to our filter date. Fade out the row if
+//     // it is not equal to the filter.
+//     $('.log > .timestamp > .inner-date').each(function(i, obj)
+//     {
+//         var rowParent = $(this).parent().parent();
+//
+//         if ($(this).text().includes(dateToFilterBy))
+//         {
+//             rowParent.fadeIn();
+//         }
+//         else
+//         {
+//             rowParent.fadeOut();
+//         }
+//     });
+// });
+$(document).ready(function()
 {
-    // Get the datepicker value
-    var dateToFilterBy = $("#date-time-picker").val();
 
-    // For each log's timestamp, check if the date of the row
-    // is equal to our filter date. Fade out the row if
-    // it is not equal to the filter.
-    $('.log > .timestamp > .inner-date').each(function(i, obj)
-    {
-        var rowParent = $(this).parent().parent();
-
-        if ($(this).text().includes(dateToFilterBy))
-        {
-            rowParent.fadeIn();
-        }
-        else
-        {
-            rowParent.fadeOut();
-        }
-    });
-});
-
-$("#searchnumber").click(function(event)
-{
-  var idFilter = $("searchnumber").val();
-
-
-
-  var rowParent = $(this).parent().parent();
-
-  if ($(this).text().include(idFilter))
+  $("#searchnumber").click(function(event)
   {
-    rowParent.fadeIn();
-  }
-  else {
-    {
-      rowParent.fadeOut();
-    }
-  });
+    
+      var filternumber = $("searchid").val();
 
+
+      $('.order > .searchfornumber > .idsearch').each(function(i, obj)
+      {
+          var rowParent = $(this).parent().parent();
+
+          if ($(this).text().includes(filternumber))
+          {
+              rowParent.fadeIn();
+          }
+          else
+          {
+              rowParent.fadeOut();
+          }
+      });
+  });
 });
